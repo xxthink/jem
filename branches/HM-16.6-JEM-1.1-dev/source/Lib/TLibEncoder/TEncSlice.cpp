86ff8610560576f92185bddca68e4eba4591977d
lxiang <lxiang@5c448f32-3df2-4d08-80fa-4e6150922d00>
2016-03-01 21:42:55 +0000

    VCEG_AZ07_INIT_PREVFRAME_FIX for gop16

--- branches/HM-16.6-JEM-1.1-dev/source/Lib/TLibEncoder/TEncSlice.cpp
+++ branches/HM-16.6-JEM-1.1-dev/source/Lib/TLibEncoder/TEncSlice.cpp
@@ -1217,6 +1217,9 @@ Void TEncSlice::encodeSlice   ( TComPic* pcPic, TComOutputBitstream* pcSubstream
     pEncBin->freeMemoryforBinStrings();
   }
 #endif
+#if VCEG_AZ07_INIT_PREVFRAME_FIX
+  pcSlice->updateStatsGlobal();
+#endif
 }
 
 Void TEncSlice::calculateBoundingCtuTsAddrForSlice(UInt &startCtuTSAddrSlice, UInt &boundingCtuTSAddrSlice, Bool &haveReachedTileBoundary,
