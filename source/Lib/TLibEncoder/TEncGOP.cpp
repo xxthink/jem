e765e9295ba051ff1e31f61ad9a7210e8d8c63d9
lxiang <lxiang@5c448f32-3df2-4d08-80fa-4e6150922d00>
2017-02-01 22:06:44 +0000

    JCTVC-Z0083 add start code bytes (Annex B) into rate calculation. Patch provided by karsten.suehring@hhi.fraunhofer.de

--- source/Lib/TLibEncoder/TEncGOP.cpp
+++ source/Lib/TLibEncoder/TEncGOP.cpp
@@ -2526,7 +2526,6 @@ Void TEncGOP::xCalculateAddPSNR( TComPic* pcPic, TComPicYuv* pcPicD, const Acces
 
 
   /* calculate the size of the access unit, excluding:
-   *  - any AnnexB contributions (start_code_prefix, zero_byte, etc.,)
    *  - SEI NAL units
    */
   UInt numRBSPBytes = 0;
@@ -2540,6 +2539,15 @@ Void TEncGOP::xCalculateAddPSNR( TComPic* pcPic, TComPicYuv* pcPicD, const Acces
     if ((*it)->m_nalUnitType != NAL_UNIT_PREFIX_SEI && (*it)->m_nalUnitType != NAL_UNIT_SUFFIX_SEI)
     {
       numRBSPBytes += numRBSPBytes_nal;
+    }
+    // add start code bytes (Annex B)
+    if (it == accessUnit.begin() || (*it)->m_nalUnitType == NAL_UNIT_VPS || (*it)->m_nalUnitType == NAL_UNIT_SPS || (*it)->m_nalUnitType == NAL_UNIT_PPS)
+    {
+      numRBSPBytes += 4;
+    }
+    else
+    {
+      numRBSPBytes += 3;
     }
   }
 
