4dcf56a2d599af9373505e321f88df235a347489
lxiang <lxiang@5c448f32-3df2-4d08-80fa-4e6150922d00>
2017-05-30 20:29:33 +0000

    cleaned FIX_TICKET20

--- source/Lib/TLibEncoder/TEncSearch.cpp
+++ source/Lib/TLibEncoder/TEncSearch.cpp
@@ -1815,13 +1815,8 @@ TEncSearch::xEncIntraHeader( TComDataCU*  pcCU,
 #if VCEG_AZ05_INTRA_MPI 
         m_pcEntropyCoder->encodeMPIIdx(pcCU, 0, true);
 #endif
-#if COM16_C1046_PDPC_INTRA && !FIX_TICKET20
-        m_pcEntropyCoder->encodePDPCIdx( pcCU, 0, true);
-#endif
       }
-#if COM16_C1046_PDPC_INTRA && FIX_TICKET20
       m_pcEntropyCoder->encodePDPCIdx( pcCU, 0, true );
-#endif
 #if JVET_C0024_QTBT
       if (pcCU->isIntra(0) )
 #else
